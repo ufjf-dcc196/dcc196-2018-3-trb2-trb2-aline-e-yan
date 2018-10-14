33f0a942665478d94cf4c098b15e58a09a9c83ab
Aline Sotte <alinedpaso@gmail.com>
2018-10-13 22:11:28 -0300

    Tela DetalhesPessoas, Listar Pessoas e Eventos

---
+++ app/src/main/java/com/example/yan/apptrabalho1/ListarEventosActivity.java
@@ -0,0 +1,13 @@
+package com.example.yan.apptrabalho1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListarEventosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_eventos);
+    }
+}
