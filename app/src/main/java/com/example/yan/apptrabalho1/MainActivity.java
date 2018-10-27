1ed8805bb6179939510f71354e062df576180c2a
YanPaiva <gckael@gmail.com>
2018-10-27 15:33:27 -0300

    Eventos no Botão da activity principal

--- app/src/main/java/com/example/yan/apptrabalho1/MainActivity.java
+++ app/src/main/java/com/example/yan/apptrabalho1/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.yan.apptrabalho1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+     private RecyclerView mainprincipal;
+     private Button cadparticipante, cadevento, listeventos,detalpessoas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainprincipal=(RecyclerView)findViewById(R.id.recprincipal);
+
+        cadparticipante=(Button)findViewById(R.id.btncadastrarpessoa);
+
+        cadparticipante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1 = new Intent(MainActivity.this,CadastrarParticpanteActivity.class);
+                startActivity(intent1);
+
+            }
+        });
+
+        cadevento=(Button)findViewById(R.id.btncadastrarevento);
+
+        cadevento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             Intent intent2= new Intent(MainActivity.this,CadastroEventoActivity.class);
+             startActivity(intent2);
+            }
+        });
+
+        listeventos=(Button)findViewById(R.id.btnlistaevento);
+        listeventos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(MainActivity.this, ListarEventosActivity.class);
+                startActivity(intent3);
+            }
+        });
+
+        detalpessoas=(Button)findViewById(R.id.btndetalhespessoas);
+        detalpessoas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4 = new Intent(MainActivity.this, DetalhesPessoasActivity.class);
+                startActivity(intent4);
+            }
+        });
+
+
+
+
+
     }
 }
